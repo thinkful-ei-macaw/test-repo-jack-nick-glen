1b4d203326b8a3aefcef52b68779c3e7ea04b4b5
Jack Lansing <jack.lansing@live.com>
2020-02-10 16:12:37 -0500

    completed problems through 8

--- function-drills1.js
+++ function-drills1.js
@@ -1,10 +1,25 @@
 'use strict';
-function getYearOfBirth(age){
-    return 2020 - age;
+
+function getYearOfBirth(age) {
+    let result = 2020 - age;
+    if (result < 0) {
+        throw new Error("Age cannot be negative");
+    } else {
+        return result;
+    }
 }
+
 function createGreeting(name, age) {
-    const yob = getYearOfBirth(age);
-    return `Hi my name is ${name} and I'm ${age}. I was born in ${yob}`;
+    if (typeof name === 'string' || typeof age === 'number') {
+        throw new Error("Arguments not valid");
+    } else {
+        const yob = getYearOfBirth(age);
+        return `Hi my name is ${name} and I'm ${age}. I was born in ${yob}`;
+    }
 }
-const greeting1 = createGreeting('Jack', 999);
-console.log(greeting1);
+try {
+    const greeting1 = createGreeting('Jack', "blargh");
+    console.log(greeting1);
+} catch (e) {
+    console.log(e.message);
+}
